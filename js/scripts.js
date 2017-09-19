9a2596557831fa0907f05d02d66d2c0d8c2deffd
angel917k <angel917k@gmail.com>
2017-09-18 21:07:43 -0700

    Take home Day 1

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+
+// Take home for day 1.
+  console.log("I expect to get as much knowledge out of this class as possible. I expect to overload on anything javascript related. This class will be the make or break for me to see if this is the career path I want to take, and I really believe this is what I want.");
