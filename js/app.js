2b0472918a735f0ef55d0c241b98b43d7504bfa0
angel917k <angel917k@gmail.com>
2017-09-25 12:18:17 -0700

    fixed grammar for day 2 hw

--- js/app.js
+++ js/app.js
@@ -5,17 +5,17 @@ console.log(userFirstName);
 
 // Asks for user's last name and alerts it
 var userLastName = prompt("What is your last name?");
-alert("Your last name is " + userLastName);
+alert("Your last name is " + userLastName + ".");
 
-// Asks for age and alerts confim of correct response
+// Asks for age and alerts confim of correct  response
 var userBirthday = prompt("When is your birthday?");
 var ageResponse;
-var confirmation = confirm("Press okay if your birthday is on " + userBirthday + ". Press cancel if your birthday is incorrect");
+var confirmation = confirm("Press okay if your birthday is on " + userBirthday + ". Press cancel if your birthday is incorrect.");
 if (confirmation == true) {
     ageResponse = alert("Your birthday is on " + userBirthday + ".");
 } else {
     var userBirthday = prompt("When is your birthday?");
-    var confirmation = confirm("Press okay if your birthday is on " + userBirthday + ". Press cancel if your birthday is incorrect");
+    var confirmation = confirm("Press okay if your birthday is on " + userBirthday + ". Press cancel if your birthday is incorrect.");
     if (confirmation == true) {
         ageResponse = alert("Your birthday is on " + userBirthday + ".");
     } else {
